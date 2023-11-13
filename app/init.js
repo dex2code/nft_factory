6f0dfef2abeaa2d9bcc80c42e08fdf79d9f6efd7
dex2code <dex2code@localhost>
2023-11-13 16:11:40 +0300

    commit

--- app/init.js
+++ app/init.js
@@ -8,9 +8,22 @@ $('#copy-smart-contract').on('click', async function() {
     try {
         await navigator.clipboard.writeText(appSmartContract);
     } catch(err) {
-        logger('warning', `Cannot copy text '${appSmartContract}' to clipboard`);
+        logger('warning', `Cannot copy text '${appSmartContract}' to clipboard (${err})`);
+
+        $('#toast-body').removeClass('bg-success');
+        $('#toast-body').removeClass('bg-danger');
+        $('#toast-body').addClass('bg-danger');
+        $('#toast-text').text('Error copying to clipboard, check your browser permissions!');
+        $('.toast').toast('show');
+    
         return;
     }
+
+    $('#toast-body').removeClass('bg-success');
+    $('#toast-body').removeClass('bg-danger');
+    $('#toast-body').addClass('bg-success');
+    $('#toast-text').text('Copied SC address to clipboard!');
+    $('.toast').toast('show');
 
     logger('debug', `Copied text '${appSmartContract}' to clipboard`);
 
--- app/last.js
+++ app/last.js
@@ -1,4 +1,4 @@
+//Enable ToolTips
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-});
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) { return new bootstrap.Tooltip(tooltipTriggerEl); });
+
